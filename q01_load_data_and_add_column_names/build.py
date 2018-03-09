acf61f6c61c154c68197d63e7c01b5cd57c88b55
ms10031990 <ms10031990@yahoo.com>
2018-03-09 15:46:00 +0000

    Done

--- q01_load_data_and_add_column_names/build.py
+++ q01_load_data_and_add_column_names/build.py
@@ -1,6 +1,15 @@
 import pandas as pd
-
+import numpy as np
 
 path = 'data/GermanData.csv'
 
-def q01_load_data_and_add_column_names():
+def q01_load_data_and_add_column_names(path):
+    df  = pd.read_csv(path)
+    #print df.head(5)
+    df.columns = ['account_status', 'month', 'credit_history', 'purpose', 'credit_amount', 'savings_account/bonds',
+                    'employment', 'installment_rate', 'personal_status/sex', 'guarantors', 'residence_since',
+                    'property', 'age', 'other_installment_plans', 'housing', 'number_of_existing_credits', 'job', 'liable', 'telephone', 'foreign_worker', 'good/bad']
+
+    df["good/bad"] = np.where(df["good/bad"]==1 ,  0 ,1)
+
+    return df
