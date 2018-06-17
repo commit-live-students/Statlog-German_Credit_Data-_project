60afb6c47bd184e0c946ffb1e45206f48365e0b7
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-06-17 11:28:02 +0000

    Done

--- q07_randomsearch_predict/build.py
+++ q07_randomsearch_predict/build.py
@@ -7,5 +7,14 @@ from greyatomlib.statlog_german_credit_data_project.q06_feature_preprocessing.bu
 
 path = 'data/GermanData.csv'
 
-def q07_randomsearch_predict():
-    
+def q07_randomsearch_predict(path):
+    X_train,X_test,y_train,y_test = q06_feature_preprocessing(path,kind='regular',random_state=9,k=8)
+    param_distributions = {'n_estimators':[100,150,200],'max_depth':[3,4,5],'min_samples_split':[2,3,4]}
+    model = GradientBoostingClassifier()
+    search = RandomizedSearchCV(model,param_distributions)
+    search.fit(X_train,y_train)
+    y_pred = search.predict(X_test)
+    auc_score = roc_auc_score(y_test,y_pred)
+    return auc_score
+
+#q07_randomsearch_predict(path)
