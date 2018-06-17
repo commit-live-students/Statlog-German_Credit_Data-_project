5e170d14c05867a46272bafeac609ce5d016a804
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-06-17 08:43:29 +0000

    Done

--- q03_enc_features/build.py
+++ q03_enc_features/build.py
@@ -6,5 +6,18 @@ from greyatomlib.statlog_german_credit_data_project.q01_load_data_and_add_column
 
 path = 'data/GermanData.csv'
 
-def q03_encode_features():
+def q03_encode_features(path):
+    df = q01_load_data_and_add_column_names(path)
+    df1 = df.iloc[1:,:].copy(deep=True)
+    d = {}
+    i = 0
+    le = LabelEncoder()
+    for val in list(df.columns):
+        if df[val].dtype == 'object':
+            d[i] = val
+            df1[val] = le.fit_transform(df1[val])
+            i = i + 1
 
+    return df1,d
+
+#q03_encode_features(path)
