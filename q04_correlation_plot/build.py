3875a755a780c9393150252c1b35fe3f94b646ca
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-06-17 10:07:59 +0000

    Done

--- q04_correlation_plot/build.py
+++ q04_correlation_plot/build.py
@@ -5,5 +5,10 @@ import sys, os
 from greyatomlib.statlog_german_credit_data_project.q03_encode_features.build import q03_encode_features
 path = 'data/GermanData.csv'
 
-def q04_correlation_plot():
-   
+def q04_correlation_plot(path):
+    df = q03_encode_features(path)[0]
+    corr1 = df.corr()
+    sns.heatmap(corr1)
+    plt.show()
+
+q04_correlation_plot(path)
