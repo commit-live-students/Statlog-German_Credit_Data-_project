2fcf83a503ebf4b939dd17ab3c0feb56df656bed
SL345 <sherilyn345@gmail.com>
2018-07-29 15:24:33 +0000

    Done

--- q02_plot_all_variable/build.py
+++ q02_plot_all_variable/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot_all_variable/build.py
 import pandas as pd
 import numpy as np
 import sys, os
@@ -8,5 +9,11 @@ from greyatomlib.statlog_german_credit_data_project.q01_load_data_and_add_column
 
 path = 'data/GermanData.csv'
 
-def q02_plot_all_variable():
-    
+def q02_plot_all_variable(path,d=5):
+    df = q01_load_data_and_add_column_names(path)
+    num = df.select_dtypes(['int64']).columns
+    for i in range(0,len(num)):
+        for j in range(0,len(num)):
+            plt.plot(df[num[i]],df[num[j]])
+    plt.show()
+
