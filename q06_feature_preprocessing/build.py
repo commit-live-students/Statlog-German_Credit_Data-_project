9f0cda03ef4289c57fc7eaef3887eb3204e1f69f
Nikhil Nair <nnair25@gmail.com>
2018-08-16 14:28:37 +0530

    Delete old files

--- q06_feature_preprocessing/build.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.feature_selection import SelectKBest
-from imblearn.over_sampling import SMOTE
-from greyatomlib.statlog_german_credit_data_project.q05_split.build import q05_split
-path = 'data/GermanData.csv'
-
